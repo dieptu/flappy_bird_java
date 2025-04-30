acd88ec3489b73072852df80ab88bca7ed7f1176
tuminhdiep <tuminh2307@gmail.com>
2025-04-30 18:14:33 -0400

    import all assets

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        
+    }
+}
